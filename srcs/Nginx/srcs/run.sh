bdff3893ecd5350dfb49f8cdb2d24fc13dc752af
zjamali <zouhairjamali1998@gmail.com>
2021-02-06 16:58:16 +0100

    add php_my_admin server

--- srcs/Nginx/srcs/run.sh
+++ srcs/Nginx/srcs/run.sh
@@ -1,5 +1,9 @@
 rc-status
 touch /run/openrc/softlevel
 rc-service nginx restart
+rc-service sshd restart
 rc-service nginx status
-/bin/sh
+rc-service sshd status
+while true
+do	/bin/sh
+done
---
+++ srcs/PhpMyAdmin/srcs/run.sh
@@ -0,0 +1,9 @@
+rc-status
+rc-service nginx restart
+rc-service nginx status
+rc-service php-fpm7 restart
+rc-status
+/bin/sh
+#while true
+#do	/bin/sh
+#done
