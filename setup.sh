20817d1db95f196c4d282a1d9d8d5689e194a079
zjamali <zouhairjamali1998@gmail.com>
2021-02-08 17:54:58 +0100

    add metlb

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-#
+# install metal lb
+echo "!!! installing metallb "
+sh srcs/setup_metallb.sh
+
 # building nginx image
+
 echo "!!!building nginx image"
 cd srcs/Nginx
 docker build -t my/nginx .
---
+++ srcs/setup_metallb.sh
@@ -0,0 +1,5 @@
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb_config.yaml
