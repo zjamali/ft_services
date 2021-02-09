a2a7a7ef4562acf53cbd2f4af9fe0721731494b1
zjamali <zouhairjamali1998@gmail.com>
2021-02-09 20:03:42 +0100

    add php my admin

--- setup.sh
+++ setup.sh
@@ -6,5 +6,18 @@ sh srcs/setup_metallb.sh
 
 echo "!!!building nginx image"
 cd srcs/Nginx
-docker build -t my/nginx .
+docker build -t nginx-image .
+
+# building nginx image
+cd ../..
+echo "!!!building mysql image"
+cd srcs/MySQL
+docker build -t mysql-image .
+
+# building nginx image
+cd ../..
+echo "!!!building phpmyadmin image"
+cd srcs/PhpMyAdmin
+docker build -t phpmyadmin-image .
+
 
--- srcs/MySQL/srcs/run.sh
+++ srcs/MySQL/srcs/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 rc-status
+
+#mysql -u root -e "SET PASSWORD FOR 'zjamali'@'localhost' = PASSWORD('passwd')";
+mysql -u root -e "CREATE USER 'zjamali'@'localhost' IDENTIFIED BY 'Zouhair'";
+mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost'";
+mysql -u root -e "FLUSH PRIVILEGES";
+sed 's/skip-networking/#skip-networking/' /etc/my.cnf.d/mariadb-server.cnf
 rc-service mariadb restart
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passwd')";
 rc-status 
 while true
 do	/bin/sh
