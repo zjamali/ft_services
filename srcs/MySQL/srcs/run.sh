24ae7ccb50053704997f3769326937305f8c3992
zjamali <zouhairjamali1998@gmail.com>
2021-02-18 13:01:45 +0100

    adding supervisor to all except mysql

--- srcs/MySQL/srcs/run.sh
+++ srcs/MySQL/srcs/run.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
- /etc/init.d/mariadb setup
-sleep 2
+/etc/init.d/mariadb setup
 rc-service mariadb start
-sleep 2
 mysql -u root -e "CREATE DATABASE wp"; 
 mysql -u root -e "CREATE USER 'zjamali'@'%' IDENTIFIED BY 'Zouhair'"; 
 mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'zjamali'@'%'"; 
 mysql -u root -e "FLUSH PRIVILEGES";
-mysql -u root wp < wp.sql
+mysql -u root wp < /wp.sql
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-rc-service mariadb stop
-rc-service mariadb start
-sleep 5
-#fi
 rc-service mariadb restart
-sleep 2
 rc-service telegraf start
 while true
 do	/bin/sh
