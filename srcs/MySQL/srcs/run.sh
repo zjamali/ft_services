ffc7a5a1b29282d2d49e140be841ca2bfae393d2
zjamali <zouhairjamali1998@gmail.com>
2021-02-19 17:54:47 +0100

    adding supervisor to mysql

--- srcs/MySQL/srcs/run.sh
+++ srcs/MySQL/srcs/run.sh
@@ -7,8 +7,6 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'zjamali'@'%'";
 mysql -u root -e "FLUSH PRIVILEGES";
 mysql -u root wp < /wp.sql
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-rc-service mariadb restart
-rc-service telegraf start
-while true
-do	/bin/sh
-done
+rc-service mariadb stop
+sleep 2
+/usr/bin/supervisord -c /etc/supervisord.conf
